aaa0689f438d6cba46901b478b362ae7bdd19fcb
Jikai Yin <sjtudyyjk@126.com>
2014-07-30 10:59:33 -0700

    Update config.php
    delete my own password...

--- include/config.php
+++ include/config.php
@@ -4,7 +4,7 @@
  */
 define("DB_HOST","localhost");
 define("DB_USER", "root");
-define("DB_PASSWORD","13593006626");
+define("DB_PASSWORD","xxxxxxxx");
 //TODO create the database and put name here
 define("DB_DATABASE","nearby_location_api");
 
